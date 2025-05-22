b80891cde6147d0691f62c9cc0f16ec8eb74e6c7
Patrick Raedler <code@readpato.dev>
2025-05-22 15:50:16 +0200

    refactor(hello-word): separate function into smaller ones, declare multiple consts at once

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,31 +2,37 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
-const frenchHelloPrefix = "Bonjour, "
-const germanHelloPrefix = "Hallo, "
-const spanish = "spanish"
-const french = "french"
-const german = "german"
+const (
+	french  = "french"
+	german  = "german"
+	spanish = "spanish"
+
+	englishHelloPrefix = "Hello, "
+	frenchHelloPrefix  = "Bonjour, "
+	germanHelloPrefix  = "Hallo, "
+	spanishHelloPrefix = "Hola, "
+)
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "world"
 	}
 
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
 
+func greetingPrefix(language string) (prefix string) {
 	switch language {
-		case spanish:
-			prefix = spanishHelloPrefix
-		case french:
-			prefix = frenchHelloPrefix
-		case german:
-			prefix = germanHelloPrefix
- }
-
-	return prefix + name
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	case german:
+		prefix = germanHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
