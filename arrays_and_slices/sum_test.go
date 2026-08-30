020d26690d7146f711573b534ed5a6745ba453cf
Patrick Raedler <code@readpato.dev>
2026-08-30 12:17:09 +0200

    feat(arrays_and_slices): expand more, create tests and MOOOORE

--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -1,6 +1,7 @@
 package arraysandslices
 
 import (
+	"slices"
 	"testing"
 )
 
@@ -15,4 +16,29 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d, given %v", got, want, numbers)
 		}
 	})
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sum of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{3, 4})
+		want := []int{2, 4}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
+	})
+
 }
